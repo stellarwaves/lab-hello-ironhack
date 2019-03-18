3a38358e321a34ed2406fc2c8065c1a456629d30
stellarwaves <matias.salasdev@gmail.com>
2019-03-18 16:22:28 +0100

    css

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import Card from './components/Card';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          <header class="header">
+            <div class="row">
+              <div class="inner-header">
+                <img class="logo" src="images/ironhack-logo.svg" alt="img"/>
+                <nav>
+                  <img class="menu-icon" src="images/menu-top.svg" alt="img"/>
+                </nav>
+              </div>
+            </div>
+          </header>
+          <section class="hero">
+            <div class="row">
+              <div class="inner-hero">
+                <div class="hero-content">
+                  <h1>Say hello to React JS</h1>
+                  <p>You will learn a Frontend framework from scratch,to become Ninka developer</p>
+                  <a href={'/'}>Awesome!</a>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="info">
+            <div class="row">
+              <div class="inner-info">
+                <article class="card">
+                  <img class="logo" src="images/icon1.png" alt="img"/>
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+                </article>
+                <article class="card">
+                  <img class="logo" src="images/icon2.png" alt="img"/>
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+                </article>
+                <article class="card">
+                  <img class="logo" src="images/icon3.png" alt="img"/>
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+                </article>
+                <article class="card">
+                <img class="logo" src="images/icon4.png" alt="img"/>
+                  <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+                </article>
+              </div>
+            </div>
+          </section>
+        </div>
     );
   }
 }
---
+++ lab-hello-react/src/components/Card.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './card.css';
+
+class Card extends Component {
+   render(){
+ 
+   }
+}
+
+
+export default Card
